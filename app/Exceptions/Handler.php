18b50817177aeaa93f4ba0c9afb69dbb573fc1b1
= <=>
2022-05-07 12:36:21 +0700

    praktikum2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundexception;
+use illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -46,5 +48,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, $exception)
+    {
+        if($exception instanceof ModelNotFoundException && $request->wantsJson()){
+            return response()->json(
+                ['message' => 'Not Found!'],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        return parent::render($request, $exception);
     }
 }
