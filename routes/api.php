73d1286a359984cab9be0306d0a72af991186fcc
= <=>
2022-05-07 12:34:24 +0700

    pertemuan2

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/todos', TodoController::class);
 });
